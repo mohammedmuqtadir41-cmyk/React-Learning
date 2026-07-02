558d368ad5c1710c9f28a726710fe51ce9d037cb
Muqthadir <mohammedmuqtadir41@gmail.com>
2026-07-02 20:38:59 +0530

    function

--- day-0/main.js
+++ day-0/main.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-aimport React from "react";
-import ReactDOM from "react-dom/client";
-
 function App() {
   return (
     <div className="container">
